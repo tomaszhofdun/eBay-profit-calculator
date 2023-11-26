76cf4deef1e2aaf5a16f8f97536eb09af6a85702
tomaszhofdun <tomaszhofdun@gmail.com>
2023-11-26 23:41:19 +0100

    Check .env

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -4,6 +4,9 @@ const apiClient = {
 
   async get(url) {
     const apiUrl = new URL(`${this.baseUrl}/${url}`)
+    console.log(this.baseUrl, "color: #ffac40")
+
+    // const apiUrl = new URL(`https://zesty-griffin-ec1b47.netlify.app//dasa`)
 
     return await fetch(apiUrl, { headers: this.headers })
   },
